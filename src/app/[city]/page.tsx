d63b5ea00e2b70f260f6806a7c1cb6862f50d8e3
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2024-11-25 14:14:55 +1100

    adjust gradient and padding

--- src/app/[city]/page.tsx
+++ src/app/[city]/page.tsx
@@ -26,16 +26,16 @@ export default async function Page({
     console.log("Current weather", currentWeather)
 
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-      <div className="bg-white rounded-lg shadow-lg p-6">
-        <SearchForm />
-        <WeatherDisplay 
-          city={city}
-          currentWeather={currentWeather}
-          forecast={fiveDayForecast}
-        />
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-300 to-purple-300">
+        <div className="bg-white rounded-lg shadow-lg p-10">
+          <SearchForm />
+          <WeatherDisplay 
+            city={city}
+            currentWeather={currentWeather}
+            forecast={fiveDayForecast}
+          />
+        </div>
       </div>
-    </div>
     )
   } catch (error) {
     return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-500 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gradient-to-br from-blue-300 to-purple-300 flex items-center justify-center p-4">
       <WeatherSearch />
     </div>
   )
